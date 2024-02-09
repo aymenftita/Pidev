c9fedcb9bdb40c0dec1d37cc4f77ccf9d3fbbb9c
Aymen Ftita <aymenftita1@gmail.com>
2024-02-09 11:21:50 +0100

    projet uploaded to github

--- Nebgha/src/main/java/com/esprit/tests/MainProg.java
+++ Nebgha/src/main/java/com/esprit/tests/MainProg.java
@@ -7,12 +7,12 @@ import com.esprit.services.EntityService2;
 public class MainProg {
 
     public static void main(String[] args) {
-        EntityService ps = new EntityService();
+        EntityService e = new EntityService();
 //        ps.ajouter(new Personne("Ahmed", "Med"));
 //        ps.supprimer(new Personne(3, "Ahmed", "Med"));
 //        ps.modifier(new Personne(1, "Emma", "Zouaoui"));
 //        System.out.println(ps.afficher());
-        EntityService2 ps2 = new EntityService2();
-        ps2.ajouter(new entity("Fedi2", "Naoufel"));
+        EntityService2 e2 = new EntityService2();
+        e2.ajouter(new entity("Fedi2", "Naoufel"));
     }
 }
