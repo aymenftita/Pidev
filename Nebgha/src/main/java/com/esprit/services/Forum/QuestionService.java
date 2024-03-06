e5d6a7086fe5bdcd87d934809d3e81f55ccca9f0
Nassim Chakroun <160069817+nassimch112@users.noreply.github.com>
2024-03-06 19:10:02 +0100

    Add files via upload

---
+++ Nebgha/src/main/java/com/esprit/services/Forum/QuestionService.java
@@ -0,0 +1,150 @@
+package com.esprit.services.Forum;
+
+import com.esprit.models.Forum.Sujet;
+import com.esprit.services.IService;
+import com.esprit.services.ServiceUtilisateur;
+import com.esprit.utils.DataSource;
+import com.esprit.models.Forum.Question;
+
+import java.sql.*;
+import java.util.*;
+
+
+public class QuestionService implements IService<Question> {
+    private Connection connection;
+
+    public QuestionService() {
+        connection = DataSource.getInstance().getConnection();
+    }
+    @Override
+    public void ajouter(Question question) {
+        String req = "INSERT into questions_forum(titre, auteur_id, date_creation, sujet_id, contenu) values ( '" +
+                 question.getTitre() + "', " + question.getAuteur().getId() +
+                ", '" + question.getDate()  + "', " +
+                question.getSujet().getId() + ", '" + question.getContenu() + "');";
+        try {
+            Statement st = connection.createStatement();
+            st.executeUpdate(req);
+            System.out.println("Question ajoutée !");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    @Override
+    public void modifier(Question question) {
+        String req = "UPDATE questions_forum set titre = '" + question.getTitre() +
+                "', auteur_id = " + question.getAuteur().getId() +
+                ", date_creation = '" + question.getDate() +
+                "', sujet_id = " + question.getSujet().getId() +
+                ", contenu = '" + question.getContenu() +
+                "' where id = " + question.getId() + ";";
+        try {
+            Statement st = connection.createStatement();
+            st.executeUpdate(req);
+            System.out.println("Question modifiée !");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @Override
+    public void supprimer(Question question) {
+        String req = "DELETE from questions_forum where id = " + question.getId() + ";";
+        try {
+            Statement st = connection.createStatement();
+            st.executeUpdate(req);
+            System.out.println("Question supprimée !");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @Override
+    public List<Question> afficher() {
+        List<Question> questions = new ArrayList<>();
+
+        String req = "SELECT * from questions_forum";
+        try {
+            Statement st = connection.createStatement();
+            ResultSet rs = st.executeQuery(req);
+            SujetService ss = new SujetService();//sujet service pour récupérer le sujet d'aprés l'ID
+            ServiceUtilisateur su = new ServiceUtilisateur();
+            while (rs.next()) {
+                questions.add(new Question(rs.getInt("id"), rs.getString("titre"),
+                        su.getUtilisateur(rs.getInt("auteur_id")), rs.getDate("date_creation"),
+                        ss.getSujet(rs.getInt("sujet_id")), rs.getString("contenu")));
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return questions;
+    }
+
+    public Question getQuestion(int id) {
+
+        Question question = null;
+
+        String req = "SELECT * FROM questions_forum WHERE id = " + id;
+
+        try {
+            Statement st = connection.createStatement();
+            ResultSet rs = st.executeQuery(req);
+
+            if (rs.next()) {  // Check if a row exists
+                SujetService ss = new SujetService();//sujet service pour récupérer le sujet d'aprés l'ID
+                ServiceUtilisateur su = new ServiceUtilisateur();
+                question = new Question(rs.getInt("id"), rs.getString("titre"),
+                        su.getUtilisateur(rs.getInt("auteur_id")), rs.getDate("date_creation"),
+                        ss.getSujet(rs.getInt("sujet_id")), rs.getString("contenu"));
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return question;
+    }
+
+
+    public List<Question> afficherParSujet(Sujet sujet) {
+        List<Question> questions = new ArrayList<>();
+
+        String req = "SELECT * from questions_forum WHERE sujet_id =" + sujet.getId();
+        try {
+            Statement st = connection.createStatement();
+            ResultSet rs = st.executeQuery(req);
+            SujetService ss = new SujetService();//sujet service pour récupérer le sujet d'aprés l'ID
+            ServiceUtilisateur su = new ServiceUtilisateur();
+            while (rs.next()) {
+                questions.add(new Question(rs.getInt("id"), rs.getString("titre"),
+                        su.getUtilisateur(rs.getInt("auteur_id")), rs.getDate("date_creation"),
+                        ss.getSujet(rs.getInt("sujet_id")), rs.getString("contenu")));
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return questions;
+    }
+
+    public int getNbrReponse(Question question) {
+
+        int count = 0;
+        String req = "SELECT COUNT(*) AS nbrReponses FROM reponse_forum WHERE question_id = " + question.getId();
+
+        try {
+            Statement st = connection.createStatement();
+            ResultSet rs = st.executeQuery(req);
+
+            if (rs.next()) {  // Check if a row exists
+                count = rs.getInt("nbrReponses");
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return count;
+
+    }
+
+}
